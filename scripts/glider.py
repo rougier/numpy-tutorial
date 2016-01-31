d7c45175ed0c6aaac4b078c47bf53f31f6ad73c6
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2016-01-31 02:08:52 -0500

    Remove shebangs and encoding comments.

--- scripts/glider.py
+++ scripts/glider.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
 # Copyright INRIA
 # Contributors: Nicolas P. Rougier (Nicolas.Rougier@inria.fr)
